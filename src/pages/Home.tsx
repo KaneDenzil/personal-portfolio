10c2e164d5d999855fb2a5416233e038fbb8d6b4
Kane Bernard <kaneon95@gmail.com>
2025-11-22 16:27:33 -0500

    Configured Global css

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import type { FC } from "react"
+
+export const Home:FC = () => {
+    return (
+        <div className="min-h-screen ">
+
+            {/* Theme Toggle */}
+
+            {/* Background Effects */}
+
+            {/* Navbar */}
+
+            {/* Main Content */}
+
+            {/* Footer */}
+            
+        </div>
+    )
+
+}
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react"
+
+export const NotFound: FC = () => {
+    return (
+        <div> Not Found Page</div>
+    )
+
+}
