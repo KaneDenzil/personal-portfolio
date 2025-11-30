3157c707fac277cbe8f14c947cd289a97f86da54
Kane Bernard <kaneon95@gmail.com>
2025-11-30 15:09:28 -0500

    Updated Config

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter basename="/personal-portfolio">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personal-portfolio/",
-    plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
